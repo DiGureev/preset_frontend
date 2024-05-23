2abaf176f39d0e6f0450cf39b28d521753570603
DiGureev <dianagureev@gmail.com>
2024-05-23 21:10:59 +0300

    retrieve csrf cookie

--- src/components/Registration.js
+++ src/components/Registration.js
@@ -118,7 +118,8 @@ const Registration = () => {
         })
         .then(response => response.json())
         .then(data => {
-            const csrfToken = data.csrfToken;
+            var csrftoken = getCookie('csrftoken');
+            // const csrfToken = data.csrfToken;
             console.log('This is token =>', csrfToken)
 
             register(body, csrfToken)
@@ -127,6 +128,21 @@ const Registration = () => {
         .catch(error => {
             console.error('Error fetching CSRF token:', error);
         });
+    }
+
+    function getTokenFromCookie(name) {
+        var cookieValue = null;
+        if (document.cookie && document.cookie !== '') {
+            var cookies = document.cookie.split(';');
+            for (var i = 0; i < cookies.length; i++) {
+                var cookie = jQuery.trim(cookies[i]);
+                if (cookie.substring(0, name.length + 1) === (name + '=')) {
+                    cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+                    break;
+                }
+            }
+        }
+        return cookieValue;
     }
 
     function togglePasswordVisibility() {
